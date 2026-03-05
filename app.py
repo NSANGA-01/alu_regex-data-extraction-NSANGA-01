a67d1e3f45c1e8daf7bbe18bb5892c9573c422fe
NSANGA_01 <ngiruwonsangavedaste6@gmail.com>
2026-03-05 13:18:23 +0200

    adding the consistency comments

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+#importing the necessary libraries
 from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
+#route for the home page
 @app.route('/')
 def index():
     return render_template('extract.html')
